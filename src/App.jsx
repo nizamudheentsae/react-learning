e8047b1875c258c1f3a25e74f31905be32fe7ad3
nizamudheen <nizamudheents.ae@gmail.com>
2026-06-14 19:24:38 +0400

    Day 42 - Add and Delete Customer

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,78 @@
-import "./App.css";
-
 import { useState } from "react";
+import "./App.css";
 import CustomerCard from "./CustomerCard";
 
 function App() {
-  const [customers] = useState([
-    { id: 1, name: "Nizam", city: "Dubai" },
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+
+  const [customers, setCustomers] = useState([
+        { id: 1, name: "Nizam", city: "Dubai" },
     { id: 2, name: "Ali", city: "Abu Dhabi" },
-    { id: 3, name: "Ahmed", city: "Sharjah" },
-  ]);
+  ])
+
+
+  function addCustomer () {
+    if(name.trim() === "" || city.trim() === "") return;
+
+    const newCustomer = {
+      id: Date.now(),
+      name,
+      city
+    };
+
+    setCustomers([...customers, newCustomer]);
+
+    setName("");
+    setCity("");
+  }
+
+  function deleteCustomer (idToDelete) {
+    setCustomers (
+      customers.filter((customer) => customer.id !== idToDelete )
+    )
+
+  }
+
   return (
+
     <div>
       <h1>Customer Dashboard</h1>
       <h3>Total Customers: {customers.length}</h3>
 
-      {customers.map((customer) => (
-        <CustomerCard
-          key={customer.id}
-          name={customer.name}
-          city={customer.city}
-        />
-      ))}
+      <input 
+        type="text"
+        placeholder="Enter Name"
+        value = {name}
+        onChange={(e) => setName(e.target.value)}
+       />
+
+       <br /><br />
+
+       <input 
+        type="text"
+        placeholder="Enter Your City"
+        value = {city}
+        onChange={(e) => setCity(e.target.value)} />
+
+        <br /><br />
+
+        <button
+          onClick={addCustomer}>Add Customer
+
+        </button>
+
+        {customers.map((customer) => (
+          <CustomerCard
+            key = {customer.id}
+            id = {customer.id}
+            name = {customer.name}
+            city = {customer.city}
+            deleteCustomer = {deleteCustomer}/>
+        ))}
     </div>
-  );
+
+  )
 }
 
 export default App;
--- src/CustomerCard.jsx
+++ src/CustomerCard.jsx
@@ -1,19 +1,18 @@
+function CustomerCard({ id, name, city, deleteCustomer }) {
+  return (
+    <div
+      style={{
+        border: "1px solid black",
+        padding: "10px",
+        margin: "10px",
+      }}
+    >
+      <h2>{name}</h2>
+      <p>{city}</p>
 
-
-function CustomerCard ({name, city}) {
-    return (
-        <div
-            style={{
-                border: "1px solid black",
-                padding: "10px",
-                margin: "10px"
-            }}>
-
-                <h2>{name}</h2>
-                <p>{city}</p>
-            </div>
-
-    )
+      <button onClick={() => deleteCustomer(id)}>Delete</button>
+    </div>
+  );
 }
 
 export default CustomerCard;
